19f6439e1aac2d6b42a67996ec39b71d1a5139b9
uvg <nis16104@uvg.edu.gt>
2021-02-15 18:05:50 -0600

    delete node_modules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 8000
+app.use(cors())
+
 
 const revertString = (text) => text.split('').reverse().join('')
 
